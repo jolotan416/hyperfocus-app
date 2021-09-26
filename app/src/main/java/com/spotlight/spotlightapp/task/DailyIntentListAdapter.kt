11f8bd0e7d04307b5c86df945b88190dd1eaf8b3
Jolo Tan <jolotan416@gmail.com>
2021-09-26 21:13:32 +0800

    Create common view for task cards

--- app/src/main/java/com/spotlight/spotlightapp/task/DailyIntentListAdapter.kt
+++ app/src/main/java/com/spotlight/spotlightapp/task/DailyIntentListAdapter.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.spotlight.spotlightapp.R
 import com.spotlight.spotlightapp.data.task.Task
 import com.spotlight.spotlightapp.databinding.DailyIntentListItemBinding
+import com.spotlight.spotlightapp.view.TaskCardView
 
 class DailyIntentListAdapter(private val callback: Callback)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -39,10 +40,14 @@ class DailyIntentListAdapter(private val callback: Callback)
         if (viewType == TITLE_VIEW_TYPE) {
             TitleViewHolder(TextView(parent.context))
         } else {
-            val binding = DailyIntentListItemBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false)
+            val context = parent.context
+            val taskBinding = DailyIntentListItemBinding.inflate(
+                LayoutInflater.from(context), parent, false)
+            val taskCardView = TaskCardView(context).apply {
+                addTaskView(taskBinding.root)
+            }
 
-            ItemViewHolder(binding)
+            ItemViewHolder(taskCardView, taskBinding)
         }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -74,10 +79,13 @@ class DailyIntentListAdapter(private val callback: Callback)
         }
     }
 
-    inner class ItemViewHolder(private val binding: DailyIntentListItemBinding)
-        : RecyclerView.ViewHolder(binding.root) {
+    inner class ItemViewHolder(
+        private val cardView: TaskCardView,
+        private val taskBinding: DailyIntentListItemBinding)
+        : RecyclerView.ViewHolder(cardView) {
         fun bindData(task: Task) {
-            binding.apply {
+            cardView.cardViewStripColor = task.category.getColorInt()
+            taskBinding.apply {
                 this.task = task
 
                 root.apply {
