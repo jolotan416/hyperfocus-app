220cc635c2f6fa493c78824a75f57eefbb075ebb
Jolo Tan <jolotan416@gmail.com>
2022-01-16 16:05:12 +0800

    Configure layout of TaskFormFragment

--- app/src/main/java/com/spotlight/spotlightapp/task/TaskFormFragment.kt
+++ app/src/main/java/com/spotlight/spotlightapp/task/TaskFormFragment.kt
@@ -1,12 +1,23 @@
 package com.spotlight.spotlightapp.task
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.spotlight.spotlightapp.R
+import com.spotlight.spotlightapp.databinding.FragmentTaskFormBinding
 
 class TaskFormFragment : Fragment(R.layout.fragment_task_form) {
     companion object {
         const val TAG = "TaskFormFragment"
 
         const val TASK = "task"
+    }
+
+    private lateinit var binding: FragmentTaskFormBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentTaskFormBinding.bind(view)
     }
 }
